33c77f9dcfa299c7dd75e4e2f2b5ba2c6d37843e
harbor_@naver.com <harbor_@naver.com>
2020-12-10 17:07:25 +0900

    201210

--- WebContent/WEB-INF/sql/myboard.sql
+++ WebContent/WEB-INF/sql/myboard.sql
@@ -6,3 +6,36 @@ CREATE TABLE member (
     regdate DATE not null
 );
 SELECT * FROM member;
+
+CREATE TABLE article (
+    article_no NUMBER GENERATED AS IDENTITY,
+    write_id VARCHAR2(50) NOT NULL,
+    write_name VARCHAR2(50) NOT NULL,
+    title VARCHAR2(255) NOT NULL,
+    regdate DATE NOT NULL,
+    moddate DATE NOT NULL,
+    read_cnd NUMBER,
+    PRIMARY KEY (article_no)
+);    
+
+CREATE TABLE article_content (
+    article_no NUMBER PRIMARY KEY,
+    content VARCHAR2(4000)
+);    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
