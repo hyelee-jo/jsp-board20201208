5e8ba6c15aee0caf18ec87f2f522d3985c26b39c
harbor_@naver.com <harbor_@naver.com>
2020-12-11 16:35:21 +0900

    201211

--- WebContent/WEB-INF/sql/myboard.sql
+++ WebContent/WEB-INF/sql/myboard.sql
@@ -3,39 +3,46 @@ CREATE TABLE member (
     memberid VARCHAR2(50) PRIMARY KEY,
     name VARCHAR2(50) NOT NULL,
     password VARCHAR2(10) NOT NULL,
-    regdate DATE not null
+    regdate DATE NOT NULL
 );
 SELECT * FROM member;
-
+DROP table article;
 CREATE TABLE article (
     article_no NUMBER GENERATED AS IDENTITY,
-    write_id VARCHAR2(50) NOT NULL,
-    write_name VARCHAR2(50) NOT NULL,
+    writer_id VARCHAR2(50) NOT NULL,
+    writer_name VARCHAR2(50) NOT NULL,
+    title VARCHAR2(255) NOT NULL,
+    regdate DATE NOT NULL,
+    moddate DATE NOT NULL,
+    read_cnt NUMBER,
+    PRIMARY KEY (article_no)
+);
+SELECT * FROM article;
+SELECT * FROM article_content;
+DROP table article_content;
+CREATE TABLE article_content (
+    article_no NUMBER PRIMARY KEY,
+    content VARCHAR2(4000)
+);
+
+INSERT INTO article (write_id,......) 
+VALUES (...);
+
+-- 11g
+INSERT INTO article (article_no, write_id,......) 
+VALUES (article_seq.nextval, ...);
+
+CREATE SEQUENCE article_seq;
+CREATE TABLE article (
+    article_no NUMBER,
+    writer_id VARCHAR2(50) NOT NULL,
+    writer_name VARCHAR2(50) NOT NULL,
     title VARCHAR2(255) NOT NULL,
     regdate DATE NOT NULL,
     moddate DATE NOT NULL,
     read_cnd NUMBER,
     PRIMARY KEY (article_no)
-);    
-
-CREATE TABLE article_content (
-    article_no NUMBER PRIMARY KEY,
-    content VARCHAR2(4000)
-);    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+);
 
 
 
